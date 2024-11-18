a5573e797e84c130af74beb902d9d4325af7ca20
Gauraang24 <garryappdroid@gmail.com>
2024-11-18 20:49:16 +0530

    Hosting : Hosted File on GihHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout.jsx';
 import Home from './Pages/Home';
 
-function App() {
-
+export default function App() {
   return (
     <>
-      <Router>
+      <Router basename="/Movie-Search-App">
         <Routes>
           <Route element={<Layout />}>
-
             <Route path="/" element={<Home />} />
-            {/* <Route path="/starred" element={<StarredMovies />} /> */}
           </Route>
         </Routes>
       </Router>
     </>
-  )
+  );
 }
-
-export default App
